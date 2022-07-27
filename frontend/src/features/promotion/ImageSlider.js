0bc29c5b6712c0a8b1e39cc3e755ba66126b7a21
daivdho <david1999.hch@gmail.com>
2022-07-27 23:59:57 +0800

    Modify slide text style

--- frontend/src/features/promotion/ImageSlider.js
+++ frontend/src/features/promotion/ImageSlider.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 /*
 slide1 => Flaming Gorge Reservoir, United States
 slide2 => Abisko, Sweden
@@ -12,7 +12,62 @@ slide7 => Enjoying sunrise while car camping over a cliff in Sedona, Arizona wit
 const ImageSlider = () => {
 
     const slidesRef = useRef();
-    const imageList = ['slide7', 'slide1', 'slide2', 'slide3', 'slide4', 'slide5', 'slide6', 'slide7', 'slide1'];
+    const imageList = [
+        {
+            slide: 'slide7',
+            title: 'Abisko, Sweden',
+            body: 'The village’s northern Sámi name Ábeskovvu means “the forest by the great water”, where “the great water” refers to Scandinavia’s largest mountain lake Torneträsk.',
+            quote: '“I Followed My Heart, And It Led Me To The Northern Lights.”'
+        },
+        {
+            slide: 'slide1',
+            title: 'Willamette National Forest, USA',
+            body: 'Home to eight wilderness areas, including the popular Three Sisters Wilderness and Mt. Jefferson Wilderness, a variety of backpacking and day hiking opportunities, waterfalls, and much more!',
+            quote: '“Life Is The Best When You’re Camping With Your Loved Ones.”'
+        },
+        {
+            slide: 'slide2',
+            title: 'Ranca Upas Ciwidey, Indonesia',
+            body: 'The natural panorama that makes you feel you’re in a different world, the delightful deer sanctuary, and the morning mist make your photos here very Instagramable!',
+            quote: '“Life Is Better By The Camp Fire.”'
+        },
+        {
+            slide: 'slide3',
+            title: 'Cathedral Provincial Park, Canada',
+            body: 'A subalpine plateau of fish-filled lakes, unique geology, alpine meadows and, most importantly, plenty of hikeable summits and ridges.',
+            quote: '“The Mountains Are Calling, And I Must Go.”'
+        },
+        {
+            slide: 'slide4',
+            title: 'Niederbauen-Chulm, Switzerland',
+            body: 'A rewarding destination, offering panoramic views of the blue inlets of Lake Lucerne down below, a jagged range of peaks to the east, the perfectly pyramidal Bristen to the south, Mount Pilatus to the west and, finally, the Rigi to the north.',
+            quote: '“Camping – Because Therapy Is Expensive.”'
+        },
+        {
+            slide: 'slide5',
+            title: 'Nui Chua Chan, Vietnam',
+            body: 'Ranked as a national-level scenic monument by the Ministry of Culture, Sports and Tourism. It is the second-highest mountain in the south where we can have a good trekking and a good view from the top.',
+            quote: '“A Bonfire Is Basically Just A Nightclub In The Mountains.”'
+        },
+        {
+            slide: 'slide6',
+            title: 'Badavut Beach, Turkey',
+            body: 'The coastline of Badavut beach is covered with golden sand. It’s very clean, the surface has a mixed fraction. Turquoise and clear water sparkles in the sun.',
+            quote: '“If People Sat Outside And Looked At The Stars Each Night, I’ll Bet They’d Live A Lot Differently.”'
+        },
+        {
+            slide: 'slide7',
+            title: 'Abisko, Sweden',
+            body: 'The village’s northern Sámi name Ábeskovvu means “the forest by the great water”, where “the great water” refers to Scandinavia’s largest mountain lake Torneträsk.',
+            quote: '“I Followed My Heart, And It Led Me To The Northern Lights.”'
+        },
+        {
+            slide: 'slide1',
+            title: 'Willamette National Forest, USA',
+            body: 'Home to eight wilderness areas, including the popular Three Sisters Wilderness and Mt. Jefferson Wilderness, a variety of backpacking and day hiking opportunities, waterfalls, and much more!',
+            quote: '“Life Is The Best When You’re Camping With Your Loved Ones.”'
+        },
+    ];
     const [accumulatedWidth, setAccumulatedWidth] = useState(0);
     const [currentWidthChange, setCurrentWidthChange] = useState(0);
     const [counter, setCounter] = useState(0);
@@ -95,8 +150,8 @@ const ImageSet = ({ slidesRef, imageList, counter, handlePrev, handleNext, handl
                 <div ref={slidesRef} className='slides'>
                     {imageList.map((item, index) => {
                         return (
-                            <>
-                                <img key={index} src={`/assets/images/${item}.jpg`} alt={item}
+                            <Fragment key={index}>
+                                <img src={`/assets/images/${item.slide}.jpg`} alt={item.slide}
                                     onClick={index === counter - 1
                                         ? () => handlePrev(index) : index === counter + 1
                                             ? () => handleNext(index) : null}
@@ -108,9 +163,11 @@ const ImageSet = ({ slidesRef, imageList, counter, handlePrev, handleNext, handl
                                     ? 'slides__texts slides__texts--active'
                                     : 'slides__texts slides__texts--inactive'}
                                     style={{ transform: `translate(${currentWidthChange * index}px, 0)` }}>
-                                    <h2 className='slides__text slides__text--header'>Hello world</h2>
+                                    <h2 className='slides__text slides__text--header'>{item.title}</h2>
+                                    <p className='slides__text slides__text--body'>{item.body}</p>
+                                    <p className='slides__text slides__text--quote'>{item.quote}</p>
                                 </div>
-                            </>
+                            </Fragment>
                         );
                     })}
                 </div>
