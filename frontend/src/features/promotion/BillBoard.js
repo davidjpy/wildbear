f2b63a4cdb785048706a6fb690b2e4b8470da376
daivdho <david1999.hch@gmail.com>
2022-07-30 15:25:24 +0800

    Add why us board

--- frontend/src/features/promotion/BillBoard.js
+++ frontend/src/features/promotion/BillBoard.js
@@ -60,18 +60,24 @@ const BillBoard = () => {
                         </div>
                     </div>
                 </div>
-                {/* <h3 className='billboard__divider'>Why Buy From CampBear ?</h3> */}
-                {/* <div className='progress'>
+                <div className='categories__description'>
+                    <h3>Why You Would Want to Buy From Us ?</h3>
+                    <div className='description__divider' />
+                    <p className='description__body'>
+                        We are a passionate team who are dedicated to providing a streamlined service to our dear customers, from the very beginning stage of looking out for merch suiting their needs, to having problems after receiving the product. We aim to spread joy and happiness to all kinds of campers. Here, not only you will find a wide range of customized gears for your requirements, but you will also find all-rounded customer supports from our help centre, full of camping enthusiasts yet professionals in resolving customers' problem.
+                    </p>
+                </div>
+                <div className='progress'>
                     <h4 className='progress__header progress__header--odd'>24-Hours Customer Service</h4>
-                    <div className='progress__arrow progress__arrow--first' />
+                    <div className='progress__arrow progress__arrow--odd' />
                     <h4 className='progress__header progress__header--even'>No Extra Charges</h4>
-                    <div className='progress__arrow progress__arrow--second' />
+                    <div className='progress__arrow progress__arrow--even' />
                     <h4 className='progress__header progress__header--odd'>Low Shippment Cost</h4>
-                    <div className='progress__arrow progress__arrow--third' />
+                    <div className='progress__arrow progress__arrow--odd' />
                     <h4 className='progress__header progress__header--even'>Speedy Delivery</h4>
-                    <div className='progress__arrow progress__arrow--fourth' />
+                    <div className='progress__arrow progress__arrow--even' />
                     <h4 className='progress__header progress__header--odd'>After Sales Service</h4>
-                </div> */}
+                </div>
             </div>
         </>
     );
