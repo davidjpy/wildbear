3aa2fd98ebe47345a437f83c5f794d2111f14300
daivdho <david1999.hch@gmail.com>
2022-08-02 22:35:12 +0800

    Add new products

--- frontend/src/features/products/NewProducts.js
+++ frontend/src/features/products/NewProducts.js
@@ -1,7 +1,28 @@
 const NewProducts = () => {
+
+    const fakeData = Array(5).fill(null);
+
     return (
-        <div className='newproducts__cards'>
-            
+        <div className='newproducts'>
+            <div className='newproducts__divider'>
+                <h3>Featured Products</h3>
+                <div className='newproducts__underline' />
+            </div>
+            <div className='newproducts__cards'>
+                {fakeData.map((item) => {
+                    return (
+                        <Product />
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
+const Product = () => {
+    return (
+        <div className='newproducts__card'>
+            <img src='/assets/images/newProduct.jpg' alt='merch' className='newproducts__image' />
         </div>
     );
 }
