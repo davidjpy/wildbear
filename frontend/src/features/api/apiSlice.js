57d583b121a8e0179ad7a55d87050a520ab95523
daivdho <david1999.hch@gmail.com>
2022-09-09 21:22:39 +0800

    Add AWS Elasticbeanstalk

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://e-commerce-env.eba-ukdr3bz2.ap-east-1.elasticbeanstalk.com/' }),
     tagTypes: ['Product'],
     endpoints: builder => ({})
 });
