ca2b9656687778d065aa2417ea7a8ffe025b77cf
daivdho <david1999.hch@gmail.com>
2022-09-10 23:09:51 +0800

    Update leftmenu

--- frontend/src/features/products/LeftMeun.js
+++ frontend/src/features/products/LeftMeun.js
@@ -75,7 +75,7 @@ const LeftMeun = ({ leftMenuRef, location }) => {
         <div ref={leftMenuRef} className='leftmenu'>
             <div className='leftmenu__wrapper'>
                 <div className='leftmenu__header'>
-                    <h5 className='leftmenu__text leftmenu__text--title' style={{ display: window.innerWidth < 1000 ? 'none' : 'block' }}>CATEGORIES</h5>
+                    <h5 className='leftmenu__text leftmenu__text--title' style={{ visibility: window.innerWidth < 1000 ? 'hidden' : 'visible' }}>CATEGORIES</h5>
                 </div>
                 <div className='leftmenu__body'>
                     {menuOptions.map((item) => {
