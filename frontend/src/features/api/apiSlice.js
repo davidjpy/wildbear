bf8592d4cfb993c8453c35b2e5c16c8e4bd4d4ae
daivdho <david1999.hch@gmail.com>
2022-09-10 17:58:35 +0800

    Update AWS ELB

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://camping-gear-api.com/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://camping-gear-api.com' }),
     tagTypes: ['Product'],
     endpoints: builder => ({})
 });
