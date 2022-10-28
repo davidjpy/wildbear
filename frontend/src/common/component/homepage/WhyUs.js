143906d690997023739d00417e11a976e7e0fbd6
daivdho <david1999.hch@gmail.com>
2022-10-28 19:29:55 +0800

    Update WhyUs

--- frontend/src/common/component/homepage/WhyUs.js
+++ frontend/src/common/component/homepage/WhyUs.js
@@ -5,8 +5,7 @@ const WhyUs = ({ scrollRef }) => {
                 <h3>Why You Would Want to Buy From Us ?</h3>
                 <div className='whyus__underline' />
                 <p className='whyus__body'>
-                    We are a passionate team who are dedicated to providing a streamlined service to our dear customers, from the very beginning stage of looking out for merch suiting their needs, to having problems after receiving the product. We aim to spread joy and happiness to all kinds of campers. Here, not only you will find a wide range of customized gears for your requirements, but you will also find all-rounded customer supports from our help centre, full of camping enthusiasts yet professionals in resolving customers' problem.
-                </p>
+                    We are a passionate team that is committed to offering our valued customers a seamless experience from the moment when they are browsing for merchandise that suits their needs to having issues after receiving the product. To all types of campers, we want to spread joy and happiness. Not only will you discover a large selection of gear that is specifically designed for your needs here, but you'll also get all-around customer care from our help center, which is staffed with camping lovers who are also experts in solving customer problems.                </p>
             </div>
             <div className='whyus__progress'>
                 <h4 className='whyus__header whyus__header--odd'>24-Hours Customer Service</h4>
